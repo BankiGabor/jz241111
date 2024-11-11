4e82bf3c677494bce2562849783ff2d5e5873b04
banki.gabor <banki.gabor@28-DIAK7>
2024-11-11 13:20:29 +0100

    Add project files.

---
+++ Fish.cs
@@ -0,0 +1,73 @@
+﻿namespace jz241111;
+
+internal class Fish
+{
+    private int top;
+    private int depth;
+    private float weight;
+    private bool weightIsSet = false;
+    private string species;
+
+    public float Weight
+    {
+        get => weight; 
+        set
+        {
+            if (value < 0.5 || value > 40)
+                throw new Exception($"hibás érték: Weight := {value}\nhiba, a Weight érték [0.5; 40] intervallumon belül valid");
+
+            if (weightIsSet && (value < weight*0.9 || value > weight*1.1 ))
+                throw new Exception($"a Weight régi értéke: {weight}\na beállítani kívánt érték: {value}\nhiba, a Weight érték nem változhat nagyobbat, mint az aktuális érték 10%-a");
+
+            weight = value;
+            weightIsSet = true;
+        }
+    }
+    public bool Predator { get; }
+    public int Top
+    {
+        get => top;
+        set
+        {
+            if (value < 0 || value > 400) 
+                throw new Exception($"hibás érték: Top := {value}\nhiba, a Top érték [0; 400] intervallumon belül valid");
+            top = value;
+        }
+    }
+    public int Depth
+    {
+        get => depth; set
+        {
+            if (value < 10 || value > 400) 
+                throw new Exception($"hibás érték: Depth := {value}\nhiba, a Depth érték [10; 400] intervallumon belül valid");
+            depth = value;
+        }
+    }
+    public string Species
+    {
+        get => species; 
+        set
+        {
+            if (value is null)
+                throw new NullReferenceException("A Species nem vehet fel null értéket");
+
+            if (value.Length < 3 || value.Length > 30)
+                throw new Exception($"a kapott érték({value}) nem felel meg a kritériumoknak\nA Species string hossza [3, 30] között kell lennie");
+            species = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Species} ({(Predator ? "carnivore" : "herbivore")}) [{Top} - {Top + Depth} cm] {Weight:0.0} kg";
+    }
+
+    public Fish(float weight, bool predator, int top, int depth, string species)
+    {
+        Weight = weight;
+        Predator = predator;
+        Top = top;
+        Depth = depth;
+        Species = species ?? throw new ArgumentNullException(nameof(species));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using jz241111;
+
+List<Fish> lake = new();
+
+string[] fishspecies = ["blobfish", "trout", "eel", "sardine", "cod", "shark", "clownfish", "piranha", "pufferfish", "kardhal", "goldfish", "barracuda", "karp", "magikarp"];
+
+for (int i = 0; i < 100; i++)
+{
+    lake.Add(new(
+        species: fishspecies[Random.Shared.Next(fishspecies.Length)],
+        predator: Random.Shared.Next(0, 100) < 10,
+        weight: Random.Shared.Next(2, 80) / 2F,
+        top: Random.Shared.Next(0, 401),
+        depth: Random.Shared.Next(10, 401)
+        ));
+}
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine($"round {i + 1:00}.:");
+
+    int xindex = Random.Shared.Next(lake.Count);
+    int yindex;
+    do
+    {
+        yindex = Random.Shared.Next(lake.Count);
+    }
+    while (yindex == xindex);
+
+    var x = lake[xindex];
+    var y = lake[yindex];
+
+    Console.WriteLine("selected:");
+
+    Console.ForegroundColor = x.Predator
+        ? ConsoleColor.Red
+        : ConsoleColor.Green;
+    Console.WriteLine($"X: {x}");
+
+    Console.ForegroundColor = y.Predator
+    ? ConsoleColor.Red
+    : ConsoleColor.Green;
+    Console.WriteLine($"Y: {y}");
+
+    Console.ResetColor();
+
+    if(x.Predator != y.Predator)
+    {
+        if (x.Top > y.Top) (x, y) = (y, x);
+
+        if (y.Top < x.Top + x.Depth)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("They can meet!");
+            Console.ResetColor();
+
+            if (Random.Shared.Next(100) < 30)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The predator attacks!");
+                Console.ResetColor();
+                if (x.Predator)
+                {
+                    lake.Remove(y);
+                    if (x.Weight < 40)
+                    {
+                        x.Weight += 0.1F;
+                    }
+                }
+                else
+                {
+                    lake.Remove(x);
+                    if (y.Weight < 40)
+                    {
+                        y.Weight += 0.1F;
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("The predator is not hungry!");
+                Console.ResetColor();
+            }
+        }
+        else Console.WriteLine("They can't meet");
+    }
+    else Console.WriteLine("Can't match");
+
+    Console.WriteLine("\n--------------------------------\n");
+}
+
+Console.WriteLine($"Remaining fish population: {lake.Count}");
